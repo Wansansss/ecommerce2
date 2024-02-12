2ccefca3d892b461b00de3f926128c73f04605e3
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-12 20:39:00 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+
+import Hero from "./components/banner/Hero";
+import Container from "./components/Container";
+import { products } from "./utils/products";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
     <div>
-      Content
+      <Container>
+        <div>
+          <Hero />
+        </div>
+        <div className="m-5 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product: any, i) => {
+            return <ProductCard data={product} key={i} />;
+          })}
+        </div>
+      </Container>
     </div>
   );
 }
