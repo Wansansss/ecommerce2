ca906c89dd9912db54a1beae26feceb1c54971c5
Wansansss <ridhwanardiansyah@gmail.com>
2024-02-12 20:38:55 +0700

    Create LoginForm.tsx

---
+++ src/app/login/LoginForm.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import Heading from "../components/Heading";
+import Input from "../components/inputusers/Input";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from "../components/Button";
+import Link from "next/link";
+
+const LoginForm = () => {
+  const [isLoading, setisLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setisLoading(true);
+    console.log(data);
+    console.log("test")
+  };
+  return (
+    <>
+      <Heading title="Sign in to Sinar Lestari" />
+      <hr className="bg-slate-300 w-full h-px" />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type="password"
+      />
+      <Button
+        label={isLoading ? "Loading" : "Login"}
+        onClick={handleSubmit(onSubmit)}
+      />
+      <p className="text-sm">
+        Do not have an account?{" "}
+        <Link className="underline hover:text-red-500" href="/register">
+          Sign Up
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default LoginForm;
