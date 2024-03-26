94eb7a7e7dfd7c4611fd063429264bef53f94187
Wansansss <ridhwanardiansyah@gmail.com>
2024-03-27 03:15:22 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
+import Home from "@/components/home/Home";
+import Container from "@/components/utils/Container";
 
-import Hero from "./components/banner/Hero";
-import Container from "./components/Container";
-import { products } from "./utils/products";
-import ProductCard from "./components/products/ProductCard";
-
-export default function Home() {
+const Page =  () => {
   return (
     <div>
       <Container>
-        <div>
-          <Hero />
-        </div>
-        <div className="m-5 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product: any, i) => {
-            return <ProductCard data={product} key={i} />;
-          })}
-        </div>
+        <Home />
       </Container>
     </div>
   );
-}
+};
+
+export default Page;
