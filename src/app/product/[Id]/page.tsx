759d399748bd2a9a20a4311f76ffc0611437e9f5
Wansansss <ridhwanardiansyah@gmail.com>
2024-03-27 03:15:26 +0700

    Update page.tsx

--- src/app/product/[Id]/page.tsx
+++ src/app/product/[Id]/page.tsx
@@ -1,26 +1,50 @@
-import Container from "@/app/components/Container";
-import ProductDetails from "../ProductDetails";
+import { getProductDetail} from "@/libs/api";
+import { Rating } from "@mui/material";
 
-interface IParams {
-  productId?: string;
-}
-
-const Product = ({ params }: { params: IParams }) => {
-  console.log("params", params);
-
-  return (
-    <div className="min-h-fit
-    h-full
-    flex
-    items-center
-    justify-center
-    pb-12
-    pt-24">
-      <Container>
-        <ProductDetails />
-      </Container>
-    </div>
-  );
+const Horizontal = () => {
+  return <hr className="w-[30% my-2]" />;
 };
 
-export default Product;
+
+const Page = async ({params}:any) => {
+  const product = await getProductDetail(params.Id)
+  console.log(product);
+  return(
+    <>
+   <div className="h-full pt-44 pb-12 px-8 grid grid-cols-1 md:grid-cols-2 gap-12">
+      <div>IMAGES</div>
+      <div className="flex flex-col gap-1 text-slate-500 text-sm">
+        <h2 className="text-3xl font-medium text-black">
+          {product.data.productName}
+        </h2>
+        <div className="flex items-center gap-2">
+          <Rating value={product.data.ratingByClick} readOnly />
+          <div>{product.data.totalProductSell} Terjual</div>
+        </div>
+        <Horizontal />
+        <div className="text-justify">{product.data.productDescription}</div>
+        <Horizontal />
+        <div>
+          <span className="font-bold">CATEGORY: </span>
+          {product.data.categoryName}
+        </div>
+        <div
+          className={
+            product.data.totalProductStock ? "text-green-600" : "text-red-600"
+          }
+        >
+          {product.data.totalProductStock ? `Stok : ${product.data.totalProductStock}` : "Habis"}
+        </div>
+        <Horizontal />
+        Warna
+        <Horizontal />
+        <div>Jumlah</div>
+        <Horizontal />
+        <div>Tambah Ke Keranjang</div>
+      </div>
+    </div>
+    </>
+  )
+};
+
+export default Page;
