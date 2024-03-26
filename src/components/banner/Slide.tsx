83402ee7b6ba53642cd4d6b621023ebabf35191b
Wansansss <ridhwanardiansyah@gmail.com>
2024-03-27 03:17:49 +0700

    Create Slide.tsx

---
+++ src/components/banner/Slide.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import React from "react";
+import Container from "../utils/Container";
+
+interface propsType {
+  img: string;
+  title: string;
+  mainTitle: string;
+  price: string;
+}
+
+
+const Slide: React.FC<propsType> = ({ img, title, mainTitle, price }) => {
+  return (
+    <Container>
+      <div className="outline-none border-none relative md:max-h-[300px] sm:max-h-[200px] lg:max-h-[300px]">
+        <div className="absolute max-h-[200px] left-[30px] md:left-[70px] max-w-[250px] sm:max-w-[350px] gap-2">
+          <h3 className="text-black/80 text-[20px] lg:text-[24px]">{title}</h3>
+          <h2 className="text-black text-[16px] md:text-[26px] lg:text-[40px] font-bold leading-[1.2]">
+            {mainTitle}
+          </h2>
+
+          <h3 className="text-[20px] text-gray-500">
+            starting at{" "}
+            <b className="text-[16px] md:text-[24px] lg:text-[26px]">{price}</b>
+            .00
+          </h3>
+          <div className="bg-red-700 text-black text-[10px] md:text-[16px] p-1 px-2 rounded-lg inline-block cursor-pointer hover:bg-black hover:text-white transition-all duration-500">
+            Shop Now
+          </div>
+        </div>
+
+        <Image
+          className="w-full max-h-[250px] sm:max-h-[200px] rounded-xl object-cover"
+          src={img}
+          alt="banner"
+          width={500}
+          height={500}
+        />
+      </div>
+    </Container>
+  );
+};
+
+export default Slide;
