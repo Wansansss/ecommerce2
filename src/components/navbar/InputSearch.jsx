766c857c30ef4da48739fac5476cc83c22d8c6b8
Wansansss <ridhwanardiansyah@gmail.com>
2024-03-27 03:17:57 +0700

    Create InputSearch.jsx

---
+++ src/components/navbar/InputSearch.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
+import { IoSearchOutline } from "react-icons/io5";
+
+const InputSearch = () => {
+  const searchRef = useRef();
+  const router = useRouter();
+
+  const handleSearch = (event) => {
+    const keyword = searchRef.current.value;
+
+    if (!keyword || keyword.trim() == "") return
+
+    if (event.key === "Enter" || event.type === "click") {
+      event.preventDefault();
+
+      router.push(`/search/${keyword}`);
+    }
+  };
+
+  return (
+    <div className="relative shadow-sm items-center justify-center">
+      <input
+        placeholder="Cari..."
+        className="rounded w-full md:w-96 p-1 text-black border border-gray-600"
+        ref={searchRef}
+        onKeyDown={handleSearch}
+      />
+      <button className="absolute top-1 end-1" onClick={handleSearch}>
+        <IoSearchOutline size={24} />
+      </button>
+    </div>
+  );
+};
+
+export default InputSearch;
