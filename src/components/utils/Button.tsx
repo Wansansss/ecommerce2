3ed209e2bc01395da79630aa059bc97ccc4f9392
Wansansss <ridhwanardiansyah@gmail.com>
2024-03-27 03:18:09 +0700

    Create Button.tsx

---
+++ src/components/utils/Button.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { Icon } from "@mui/material";
+import { IconType } from "react-icons";
+
+interface ButtonProps{
+    label:string,
+    disabled?:boolean,
+    outline?:boolean,
+    small?:boolean,
+    custom?:string,
+    icon?: IconType
+    onClick: (e:React.MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button:React.FC<ButtonProps> = ({
+    label,
+    disabled,
+    outline,
+    small,
+    custom,
+    onClick,
+    icon: Icon,
+ 
+}) => {
+    
+    return ( 
+        <button 
+        disabled={disabled}
+        className={`
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-md
+        hover:opacity-80
+        transition
+        w-full
+        bg-red-600
+        border-white
+        flex
+        items-center
+        justify-center
+        gap-2
+        hover:bg-black
+        hover:text-white
+        ${outline? "bg-white" : "bg-red-700"}
+        ${outline? "text-slate-700" : "text-white"}
+        ${small? "text-sm font-semibold" : "text-md font-semibold"}
+        ${small? "py-1 px-2 border-[1px]" : "py-3 px-4 border-2"}
+        ${custom? custom : ""}
+        `}
+        >
+            {Icon && <Icon size={24}/>}
+            {label}
+        </button>
+     );
+}
+ 
+export default Button;
