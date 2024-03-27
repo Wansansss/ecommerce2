563e360bccb8c768df30227dd3208371c1d5d486
Wansansss <ridhwanardiansyah@gmail.com>
2024-03-27 20:47:04 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Quattrocento_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar/Navbar";
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Footer from "@/components/footer/Footer";
+import CartProviders from "@/providers/CartProviders";
 
 const font = Quattrocento_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -20,13 +21,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className} suppressHydrationWarning={true} >
-        <div className="flex flex-col min-h-screen">
-        <Navbar/>
-        <main className="flex-grow">{children}</main>
-        <Footer/>
-        </div>
-        
+      <body className={font.className} suppressHydrationWarning={true}>
+        <CartProviders>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-grow">{children}</main>
+            <Footer />
+          </div>
+        </CartProviders>
       </body>
     </html>
   );
