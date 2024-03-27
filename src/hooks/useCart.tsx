e7325050da9396126973e58bf29b7c9a5a9190b4
Wansansss <ridhwanardiansyah@gmail.com>
2024-03-27 20:47:23 +0700

    Create useCart.tsx

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,51 @@
+import { CartProductType } from "@/app/product/[Id]/ProductDetails";
+import { createContext, useCallback, useContext, useState } from "react";
+
+interface CartContextProps {
+  cartTotalQty: number;
+  cartProducts: CartProductType[] | null;
+  handleAddProductToCart: (product: CartProductType) => void;
+}
+
+export const CartContext = createContext<CartContextProps | null>(null);
+
+interface Props {
+  [propName: string]: any;
+}
+
+export const CartContextProvider = (props: Props) => {
+  const [cartTotalQty, setCartTotalQty] = useState(0);
+  const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(
+    null
+  );
+
+  const handleAddProductToCart = useCallback((product: CartProductType) => {
+    setCartProducts((prev) => {
+      let updatedCart;
+
+      if (prev) {
+        updatedCart = [...prev, product];
+      } else {
+        updatedCart = [product];
+      }
+      return updatedCart;
+    });
+  }, []);
+
+  const value = {
+    cartTotalQty,
+    cartProducts,
+    handleAddProductToCart,
+  };
+
+  return <CartContext.Provider value={value} {...props} />;
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+
+  if (context === null) {
+    throw new Error("useCart must be used within a CartContextProvider");
+  }
+  return context;
+};
