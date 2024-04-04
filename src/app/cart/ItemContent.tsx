49bd40151ccd5ede81f242cf36e36316904c9aa2
Wansansss <ridhwanardiansyah@gmail.com>
2024-04-05 00:32:17 +0700

    Create ItemContent.tsx

---
+++ src/app/cart/ItemContent.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { formatPrice } from "@/libs/formatPrice";
+import { CartProductType } from "../product/[Id]/ProductDetails";
+import Link from "next/link";
+import { truncateText } from "@/libs/truncateText";
+import Image from "next/image";
+import SetQuantity from "@/components/utils/products/SetQuantity";
+import { useCart } from "@/hooks/useCart";
+
+interface ItemContentProps {
+  item: CartProductType;
+}
+
+const ItemContent: React.FC<ItemContentProps> = ({ item }) => {
+  const {
+    handleRemoveProductFromCart,
+    handleCartQtyDecrease,
+    handleCartQtyIncrease,
+  } = useCart();
+
+  return (
+    <div className="grid grid-cols-5 text-sm md:text-md gap-4 border-t-[1.5px] border-red-600 items-center">
+      <div className="col-span-2 justify-self-start flex gap-2 md:gap-4 ">
+        <Link href={`/product/${item.productSecureId}`}>
+          <div className="relative w-[70px] aspect-square">
+            <Image
+              src={item.selectedImg.image}
+              alt={item.productName}
+              fill
+              className="object-contain"
+            />
+          </div>
+        </Link>
+        <div className="flex flex-col justify-between">
+          <Link href={`/product/${item.productSecureId}`}>
+            {truncateText(item.productName)}
+            <div>{item.selectedImg.color}</div>
+          </Link>
+          <div className="w-[70px]">
+            <button
+              className="text-black hover:text-red-600 underline"
+              onClick={() => handleRemoveProductFromCart(item)}
+            >
+              Hapus
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="justify-self-center">{formatPrice(item.amount)}</div>
+      <div className="justify-self-center">
+        <SetQuantity
+          cartCounter={true}
+          cartProduct={item}
+          handleQtyDecrease={() => handleCartQtyDecrease(item)}
+          handleQtyIncrease={() => handleCartQtyIncrease(item)}
+        />
+      </div>
+      <div className="justify-self-end font-bold">
+        {formatPrice(item.amount * item.qty)}
+      </div>
+    </div>
+  );
+};
+
+export default ItemContent;
