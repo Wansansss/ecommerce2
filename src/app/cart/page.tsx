dc100d441525a36fc4b5350b1ad3711888281a35
Wansansss <ridhwanardiansyah@gmail.com>
2024-04-05 00:32:21 +0700

    Update page.tsx

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,13 @@
+import Container from "@/components/utils/Container";
+import CartClient from "./CartClient";
+
 const Cart = () => {
     return ( 
-        <div className="mt-40">Cart Page</div>
+        <div className="mt-40 mx-8">
+            <Container>
+                <h1><CartClient/></h1>
+            </Container>
+        </div>
      );
 }
  
