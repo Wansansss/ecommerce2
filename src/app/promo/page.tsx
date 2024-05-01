3a8d88183522f8bcc4ca02f31e6f9d788e491111
Wansansss <ridhwanardiansyah@gmail.com>
2024-05-01 18:39:58 +0700

    Update page.tsx

--- src/app/promo/page.tsx
+++ src/app/promo/page.tsx
@@ -1,17 +1,17 @@
 import Container from "@/components/utils/Container";
 import ProductCard from "@/components/utils/products/ProductCard";
-import { getProductList } from "@/libs/api";
+import {getProductPromo } from "@/libs/api";
 
 const Page = async () => {
-    const products = await getProductList()
+    const products = await getProductPromo()
   return (
     <Container>
-      <h1 className="h-full pt-40 text-center text-2xl font-bold">Promo Spesial</h1>
-      <div className="h-full pt-6 pb-12 px-4 flex flex-row gap-12">
-        <div>Bagian Kiri</div>
-        
+      <div className="py-16">
+      <h1 className="pt-16 drop-shadow-md text-center text-4xl font-semibold">Promo Spesial</h1>
+      <hr className="bg-red-600 w-full h-2" />
           <ProductCard list={products}/>
       </div>
+     
     </Container>
   );
 };
