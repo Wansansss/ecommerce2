40d1ac2195a17a5b5f96bd9f86a89f83b7d2fa22
Wansansss <ridhwanardiansyah@gmail.com>
2024-05-01 18:40:22 +0700

    Create page.tsx

---
+++ src/app/user/dashboard/historyTransaksi/page.tsx
@@ -0,0 +1,9 @@
+const HistroryTransaksi = () => {
+    return ( 
+        <div className="pt-44 flex items-center justify-center">
+                HistoryTransaksi
+        </div>
+    );
+}
+ 
+export default HistroryTransaksi;
