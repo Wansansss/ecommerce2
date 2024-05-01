c1d917da58bf8344461f93f2b6b4ae601f1d6f6d
Wansansss <ridhwanardiansyah@gmail.com>
2024-05-01 18:39:51 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,14 @@
+import { getSession } from "@/actions/getCurrentUser";
 import Container from "../../components/utils/Container";
 import FormWrap from "../../components/utils/FormWrap";
 import LoginForm from "./LoginForm";
 
-const Login = () => {
+const Login = async () => {
+    const user = await getSession()
     return ( 
         <Container>
             <FormWrap>
-                <LoginForm/>
+                <LoginForm currentUser={user}/>
             </FormWrap>
         </Container>
 
