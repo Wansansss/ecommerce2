1769db3c40657aa1e9c6e6af7dd6c795ee11a189
Wansansss <ridhwanardiansyah@gmail.com>
2024-05-01 18:39:40 +0700

    Update page.tsx

--- src/app/kategori/[id]/page.tsx
+++ src/app/kategori/[id]/page.tsx
@@ -1,15 +1,23 @@
-import { getKategoriDetail } from '@/libs/api'
-import React from 'react'
+import ProductCard from "@/components/utils/products/ProductCard";
+import { getKategoriDetail } from "@/libs/api";
+import React from "react";
 
-const Page = async ({params}:any) => {
-    const data = await getKategoriDetail(params.id)
-    console.log(params)
-    console.log(data)
+const Page = async ({ params }: any) => {
+  const data = await getKategoriDetail(`page=1&categorySecureId=${params.id}`);
+  // console.log(data)
   return (
-    <div className='mt-44'>
-        {data.data.categoryName}
+    <div className="pt-44 px-10 font-bold">
+      {data.data?.map((list: any, i: any) => {
+        return (
+          <div key={i}>
+            <h1>Kategori: {list.categoryName}</h1>
+            <hr className="bg-red-600 h-2" />
+            <ProductCard list={data} />
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
