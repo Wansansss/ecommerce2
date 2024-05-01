ec5b5b163d22ab4a0a541c56bba4d01bd28cc814
Wansansss <ridhwanardiansyah@gmail.com>
2024-05-01 18:39:48 +0700

    Update LoginForm.tsx

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -1,37 +1,82 @@
 "use client";
 
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Heading from "../../components/utils/Heading";
 import Input from "../../components/utils/inputusers/Input";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Button from "../../components/utils/Button";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import { AiOutlineGoogle } from "react-icons/ai";
+import { SafeUser } from "@/types";
+import axios from "axios";
+import { headers } from "next/headers";
+import { User } from "next-auth";
 
-const LoginForm = () => {
+interface LoginFormProps{
+  currentUser: any
+}
+
+const LoginForm : React.FC<LoginFormProps> = ({currentUser}) => {
   const [isLoading, setisLoading] = useState(false);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FieldValues>({
-    defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-    },
+   defaultValues:{
+    username:"",
+    password:"",
+   }
   });
 
-  const onSubmit : SubmitHandler<FieldValues> = (data) => {
+  const router = useRouter();
+
+  useEffect(()=> {
+    if(currentUser){
+      router.push("/");
+      router.refresh();
+    }
+  },[])
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setisLoading(true);
-   
+      signIn("credentials", {
+      ...data,
+        redirect: false,
+      }).then((callback) => {
+        if (callback?.ok) {
+          router.push("/");
+          router.refresh();
+          toast.success("Berhasil Login");
+        }
+        if (callback?.error) {
+          toast.error(callback.error);
+        }
+      }).catch(() => toast.error("Gagal Login"))
+    .finally(() => {
+      setisLoading(false);
+    });
   };
+
+  if(currentUser){
+    return <p className="text-center">Anda Sudah Login...</p>
+  }
   return (
     <>
       <Heading title="Sign in to Sinar Lestari" />
+      <Button
+      outline
+      label="Continue With Google"
+      icon={AiOutlineGoogle}
+      onClick={() => {}}
+      />
       <hr className="bg-slate-300 w-full h-px" />
       <Input
-        id="email"
-        label="Email"
+        id="username"
+        label="Username"
         disabled={isLoading}
         register={register}
         errors={errors}
