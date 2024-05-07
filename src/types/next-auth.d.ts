b8c3946b9fa666c7a65873549ce5e446edd14bc4
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 11:34:59 +0700

    Create next-auth.d.ts

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,15 @@
+import NextAuth, { DefaultSession } from "next-auth"
+ 
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      role: string | undefined | null
+      secureId:string | undefined | null
+      fullName:string | undefined | null
+      address:string | undefined | null
+    } & DefaultSession["user"]
+  }
+}
