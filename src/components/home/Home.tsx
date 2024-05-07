0ead6509ea0cb38dd05e2efa7acdd0b9830d2646
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-08 02:53:51 +0700

    Update Home.tsx

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,18 +1,23 @@
-import { getKategoriList, getProductList } from "@/libs/api";
+import { getKategoriList, getProduct} from "@/libs/api";
 import Hero from "../banner/Hero";
 import ProductCard from "../utils/products/ProductCard";
 import KategoriCard from "../utils/kategori/KategoriCard";
 import Container from "../utils/Container";
+import NotFound from "../utils/NotFound";
 
 const Home = async () => {
-  const products = await getProductList();
+  const data = await getProduct("/list");
+  // console.log(products)
   const kategori = await getKategoriList();
+  if(data.status !== "OK"){
+      return <NotFound title="Data Tidak Ditemukan..." center/>
+  }
   return (
     <div>
       <Container>
         <Hero />
         <KategoriCard kategori={kategori} />
-        <ProductCard list={products} />
+        <ProductCard list={data} />
       </Container>
     </div>
   );
