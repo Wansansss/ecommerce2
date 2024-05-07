8f652f1f2a5675bdb43b7dd40ce320ae145a8f58
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-08 02:53:37 +0700

    Update page.tsx

--- src/app/product/promo/page.tsx
+++ src/app/product/promo/page.tsx
@@ -1,12 +1,12 @@
 import Container from "@/components/utils/Container";
 import NotFound from "@/components/utils/NotFound";
 import ProductCard from "@/components/utils/products/ProductCard";
-import {getProductPromo } from "@/libs/api";
-import { MdArrowBack } from "react-icons/md";
+import {getProduct} from "@/libs/api";
 
 const Page = async () => {
-    const products = await getProductPromo("page=1")
-    if (products.status !== "OK"){
+    const productPromo = await getProduct("/promo",`q=10`)
+    console.log(productPromo)
+    if (productPromo.status !== "OK"){
       return(
        <NotFound title="Tidak Ada Product Promo...."/>
         )
@@ -16,7 +16,7 @@ const Page = async () => {
       <div className="py-16">
       <h1 className="pt-16 drop-shadow-md text-center text-4xl font-semibold">Promo Spesial</h1>
       <hr className="bg-red-600 w-full h-2" />
-          <ProductCard list={products}/>
+          <ProductCard list={productPromo}/>
       </div>
      
     </Container>
