f1e2bbed651be99f39dbc2c0568475117199974a
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 11:32:40 +0700

    Update getCurrentUser.ts

--- src/actions/getCurrentUser.ts
+++ src/actions/getCurrentUser.ts
@@ -6,34 +6,16 @@ export async function getSession() {
     return session?.user
 }
 
-// export async function getCurrentUser() {
-//     try {
-//         const session = await getSession()
 
-//         if (!session?.user?.email) {
-//             return null
-//         }
-//         const res = await fetch("http://89.116.134.204:8011/api/sl/v1/web/users/signin",{
-//                     method: "POST",
-//                     headers: {
-//                         "Content-Type": "application/json"
-//                     },
-//                     body: JSON.stringify(session)
-//                 })
-//                 const currentUser = await res.json()
 
-//         if (!currentUser) {
-//             return null
-//         }
-
-//         return {
-//             ...currentUser,
-//             createBy: currentUser.createBy.toISOString() || null,
-//             changeBy: currentUser.updatedAt.toISOString() || null,
-//             secureId: currentUser.secureId.toString(),
-//             fullName: currentUser.fullName.toString(),
-//         }
-//     } catch (error: any) {
-//         return null
-//     }
+// export default async (req, res) => {
+//   const session = await getServerSession(req, res, authOptions)
+//   if (session) {
+//     // Signed in
+//     console.log("Session", JSON.stringify(session, null, 2))
+//   } else {
+//     // Not Signed in
+//     res.status(401)
+//   }
+//   res.end()
 // }
