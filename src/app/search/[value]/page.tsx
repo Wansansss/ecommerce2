e7e4ad3ca4971f9e5252c96ba9b8b0236aa4bd90
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 15:59:55 +0700

    Update page.tsx

--- src/app/search/[value]/page.tsx
+++ src/app/search/[value]/page.tsx
@@ -1,21 +1,28 @@
 import Heading from "@/components/utils/Heading";
+import NotFound from "@/components/utils/NotFound";
 import ProductCard from "@/components/utils/products/ProductCard";
 import { getSearchProduct } from "@/libs/api";
+import Link from "next/link";
+import { MdArrowBack } from "react-icons/md";
 
-const Search = async ({params}:any) => {
-    const keyword = params.value
-    const decodedKeyword = decodeURI(keyword)
-    const searchProduct = await getSearchProduct(decodedKeyword)
-    
-    
-    return (
-       <div className="py-16">
+const Search = async ({ params }: any) => {
+  const keyword = params.value;
+  const decodedKeyword = decodeURI(keyword);
+  const searchProduct = await getSearchProduct(decodedKeyword);
+  if (searchProduct.status !== "OK") {
+    return(
+     <NotFound title="Product Tidak Ditemukan ..."/>
+      )
+  }
+
+  return (
+    <div className="py-16">
       <div className="py-16">
         <Heading title={`Pencarian Untuk : ${decodedKeyword}...`} />
         <ProductCard list={searchProduct} />
       </div>
     </div>
-      );
-}
- 
+  );
+};
+
 export default Search;
