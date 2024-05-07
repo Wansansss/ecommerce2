f90822d50013e3bce94cf694042a27fee23ad8a3
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 11:33:18 +0700

    Update route.ts

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,19 +1,43 @@
 import { getSession } from '@/actions/getCurrentUser';
-import axios from 'axios';
 import { NextResponse } from 'next/server';
 
-export async function PUT(request:any) {
-  const body = await request.json(); 
-  const currentUser = await getSession()
-//   console.log("ini user >>>>>> " , currentUser)
+export async function PUT(request: any) {
+  const user = await getSession()
+  let secureId = ''
+  if(user?.secureId)
+   secureId = user?.secureId
+ 
+  console.log(secureId)
+  const body = await request.json()
+  console.log(body)
+  const url = "http://89.116.134.204:8011/api/sl/v1/web/users/account"
 
-
-  try {
-    const response = await axios.put(
-      'http://89.116.134.204:8011/api/sl/v1/web/users/account',body);
-    return NextResponse.json(response.data);
-  } catch (error:any) {
-    console.error('Error:', error);
+  try{
+    const res = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "x-user-secure-id" : secureId,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    }) 
+    return NextResponse.json(res)
+  } catch (error) {
+    console.log(error)
     return NextResponse.json({ message: 'Internal Server Error' });
   }
+
+    // const body = await request.json(); 
+    // const currentUser = await getSession()
+    // console.log("ini user >>>>>> " , currentUser)
+
+
+    // try {
+    //   const response = await axios.put(
+    //     'http://89.116.134.204:8011/api/sl/v1/web/users/account',body);
+    //   return NextResponse.json(response.data);
+    // } catch (error:any) {
+    //   console.error('Error:', error);
+    //   return NextResponse.json({ message: 'Internal Server Error' });
+    // }
 }
