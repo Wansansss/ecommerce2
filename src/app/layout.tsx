7c7d06c2c2d31b9c193d37549daf4338389f3108
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 11:33:28 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Footer from "@/components/footer/Footer";
 import CartProviders from "@/providers/CartProviders";
 import { Toaster } from "react-hot-toast";
-
+import Container from "@/components/utils/Container";
 
 const font = Quattrocento_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
@@ -20,27 +20,27 @@ export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) { 
+}>) {
   return (
     <html lang="en">
       <body className={font.className} suppressHydrationWarning={true}>
-          <Toaster
-            toastOptions={{
-              style: {
-                background: "rgba(0, 0, 0, 0.8)",
-                color: "#fff",
-                borderRadius: "10px",
-                padding: "10px",
-              },
-            }}
-          />
-          <CartProviders>
+        <Toaster
+          toastOptions={{
+            style: {
+              background: "rgba(0, 0, 0, 0.8)",
+              color: "#fff",
+              borderRadius: "10px",
+              padding: "10px",
+            },
+          }}
+        />
+        <CartProviders>
             <div className="flex flex-col min-h-screen">
               <Navbar />
               <main className="flex-grow">{children}</main>
               <Footer />
             </div>
-          </CartProviders>
+        </CartProviders>
       </body>
     </html>
   );
