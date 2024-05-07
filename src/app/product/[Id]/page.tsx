587e18583d2c566a7265fda69cd1e11d1e990ad2
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 15:59:42 +0700

    Update page.tsx

--- src/app/product/[Id]/page.tsx
+++ src/app/product/[Id]/page.tsx
@@ -1,10 +1,16 @@
 import Container from "@/components/utils/Container";
 import { getProductDetail} from "@/libs/api";
 import ProductDetails from "./ProductDetails";
+import NotFound from "@/components/utils/NotFound";
 
 
 const Page = async ({params}:any) => {
   const product = await getProductDetail(params.Id)
+  if(product.status !== "OK") {
+    return(
+      <NotFound title="Product Tidak Ditemukan..."/>
+    )
+  }
   return(
    <Container>
     <ProductDetails data={product.data}/>
