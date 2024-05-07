1e6d3bd7de9c47cdfe7d002717777341cac0e89d
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 11:33:35 +0700

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,15 +4,16 @@ import FormWrap from "../../components/utils/FormWrap";
 import LoginForm from "./LoginForm";
 
 const Login = async () => {
-    const user = await getSession()
-    return ( 
-        <Container>
-            <FormWrap>
-                <LoginForm currentUser={user}/>
-            </FormWrap>
-        </Container>
+  const user = await getSession();
+  return (
+    <Container>
+      <div className="py-16">
+        <FormWrap>
+          <LoginForm currentUser={user} />
+        </FormWrap>
+      </div>
+    </Container>
+  );
+};
 
-     );
-}
- 
 export default Login;
