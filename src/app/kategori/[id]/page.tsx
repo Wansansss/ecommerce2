a41cf24706c4406ccf9b3db565727fdca7f7f27a
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-07 11:33:25 +0700

    Update page.tsx

--- src/app/kategori/[id]/page.tsx
+++ src/app/kategori/[id]/page.tsx
@@ -4,19 +4,32 @@ import React from "react";
 
 const Page = async ({ params }: any) => {
   const data = await getKategoriDetail(`page=1&categorySecureId=${params.id}`);
-  // console.log(data)
+
   return (
-    <div className="pt-44 px-10 font-bold">
-      {data.data?.map((list: any, i: any) => {
-        return (
-          <div key={i}>
-            <h1>Kategori: {list.categoryName}</h1>
-            <hr className="bg-red-600 h-2" />
-            <ProductCard list={data} />
+    <>
+      <div className="pt-44 font-bold">
+        {data.data?.map((list: any, i: any) => {
+          return (
+            <div key={i}>
+              <h1>Kategori: {list.categoryName}</h1>
+              <hr className="bg-red-600 h-2" />
+            </div>
+          );
+        })}
+      </div>
+      {/* <div className="flex flex-row justify-between py-10 px-10">
+        <div className="flex flex-col px-10">
+            <h6 className="font-bold">Product Kategori</h6>
+          <div className="">
+
           </div>
-        );
-      })}
-    </div>
+        </div> */}
+        
+        <div>
+          <ProductCard list={data} />
+        </div>
+      {/* </div> */}
+    </>
   );
 };
 
