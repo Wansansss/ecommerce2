7ef0610447135961fbe30a2ddfe3884991862e55
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-09 01:42:28 +0700

    Update CartClient.tsx

--- src/app/cart/CartClient.tsx
+++ src/app/cart/CartClient.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Button from "@/components/utils/Button";
 import Heading from "@/components/utils/Heading";
 import { useCart } from "@/hooks/useCart";
@@ -7,9 +6,48 @@ import Link from "next/link";
 import { MdArrowBack } from "react-icons/md";
 import ItemContent from "./ItemContent";
 import { formatPrice } from "@/libs/formatPrice";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+import axios from "axios";
+
 
 const CartClient = () => {
-  const { cartProducts,handleClearCart,cartTotalAmount } = useCart();
+  const { cartProducts, handleClearCart, cartTotalAmount } = useCart();
+  const [loading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const router = useRouter();
+
+  const handleCheckout = async () => {
+    if (cartProducts) {
+      {
+        // cartProducts.map((item) => {
+          setIsLoading(true);
+          setError(error);
+          axios.post("/api/checkout",{
+            cartProducts
+          })
+            .then((callback) => {
+              setIsLoading(false);
+              if (callback?.status === 200) {
+                toast.success("Pesanan Telah diterima")
+                // handleClearCart()
+                // router.push("https://wa.me/6282137026688?text=Hallo Admin,Apakah pesanan saya sudah diterima?")
+              } else {
+                toast.error("Silahkan Login terlebih dahulu");
+                return router.push("/login");
+              }
+            })
+            .catch((error) => {
+              setError(true);
+              console.log(error);
+              toast.error("Terjadi Kesalahan");
+            // });
+        });
+      }
+    }
+  };
 
   if (!cartProducts || cartProducts.length === 0) {
     return (
@@ -31,20 +69,20 @@ const CartClient = () => {
   return (
     <div>
       <Heading title="Keranjang Anda" center />
-      <div className="grid grid-cols-5 sm:text-xs md:text-md md:gap-4 pb-2 mt-8 items-center">
-        <div className="col-span-2 justify-self-start">Produk</div>
-        <div className="justify-self-center">Harga</div>
-        <div className="justify-self-center">Jumlah</div>
-        <div className="justify-self-end">Total</div>
+      <div className="grid grid-cols-5 py-8 border-t-[1.5px] border-red-600 items-center">
+        <div className="col-span-1 justify-start">Produk</div>
+        <div className="col-span-1 justify-self-center">Harga</div>
+        <div className="col-span-1 justify-self-center">Jumlah</div>
+        <div className="col-span-2 justify-self-end">Total</div>
       </div>
       <div>
         {cartProducts &&
           cartProducts.map((item) => {
-            return <ItemContent key={item.productSecureId} item={item}/>
+            return <ItemContent key={item.productSecureId} item={item} />;
           })}
       </div>
       <div className="border-t-[1.5px] border-red-600 py-4 flex justify-between gap-4">
-        <div className="w-[120px]">
+        <div className="w-[80px] md:w-[120px]">
           <Button label="Hapus Semua" onClick={() => handleClearCart()} small />
         </div>
         <div className="text-md flex flex-col gap-1 items-start">
@@ -52,7 +90,7 @@ const CartClient = () => {
             <span>Total Harga</span>
             <span>{formatPrice(cartTotalAmount)}</span>
           </div>
-          <Button label="Pesan Sekarang" onClick={() => {}}/>
+          <Button label="Pesan Sekarang" onClick={() => handleCheckout()} />
           <Link
             href={"/"}
             className="text-black flex items-center gap-1 mt-2 hover:text-red-600"
