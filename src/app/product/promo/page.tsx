ea7ae4529f2e73a315378686f1f42ca3eb7cb5e8
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-09 01:42:56 +0700

    Update page.tsx

--- src/app/product/promo/page.tsx
+++ src/app/product/promo/page.tsx
@@ -1,24 +1,23 @@
 import Container from "@/components/utils/Container";
 import NotFound from "@/components/utils/NotFound";
 import ProductCard from "@/components/utils/products/ProductCard";
-import {getProduct} from "@/libs/api";
+import { getProduct } from "@/libs/api";
 
 const Page = async () => {
-    const productPromo = await getProduct("/promo",`q=10`)
-    console.log(productPromo)
-    if (productPromo.status !== "OK"){
-      return(
-       <NotFound title="Tidak Ada Product Promo...."/>
-        )
-    } 
+  const productPromo = await getProduct("/promo", `q=10`);
+  console.log(productPromo);
+  if (productPromo.status !== "OK") {
+    return <NotFound title="Tidak Ada Product Promo...." />;
+  }
   return (
     <Container>
       <div className="py-16">
-      <h1 className="pt-16 drop-shadow-md text-center text-4xl font-semibold">Promo Spesial</h1>
-      <hr className="bg-red-600 w-full h-2" />
-          <ProductCard list={productPromo}/>
+        <div className="py-16 drop-shadow-md text-center text-4xl font-semibold">
+          <h1>Promo Spesial</h1>
+          <hr className="bg-red-600 max-w-[50%] mx-auto h-1" />
+        </div>
+        <ProductCard list={productPromo} />
       </div>
-     
     </Container>
   );
 };
