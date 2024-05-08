ca1e2c44055956ed39be75d1c0559cb331b26ba2
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-09 01:42:58 +0700

    Update page.tsx

--- src/app/search/[value]/page.tsx
+++ src/app/search/[value]/page.tsx
@@ -5,18 +5,18 @@ import { getProduct} from "@/libs/api";
 const Search = async ({ params }: any) => {
   const keyword = params.value;
   const decodedKeyword = decodeURI(keyword);
-  const searchProduct = await getProduct("search",`page=1&value=${decodedKeyword}`);
+  const searchProduct = await getProduct("/search",`value=${decodedKeyword}`);
   console.log(searchProduct);
   if (searchProduct.status !== "OK") {
     return(
      <NotFound title="Product Tidak Ditemukan ..."/>
       )
   }
-
   return (
     <div className="py-16">
       <div className="py-16 px-8">
-        <Heading  title={`${decodedKeyword}...`} />
+        <Heading title={"Product"} center />
+        <hr className="w-full h-1 bg-red-700" />
         <ProductCard list={searchProduct} />
       </div>
     </div>
