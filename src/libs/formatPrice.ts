5e71002f26e641749d1c94359df990a5bc70ceee
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-09 01:43:15 +0700

    Update formatPrice.ts

--- src/libs/formatPrice.ts
+++ src/libs/formatPrice.ts
@@ -2,7 +2,13 @@ export const formatPrice = (amount: number) => {
     return new Intl.NumberFormat(
         'id-ID',{
             style:'currency',
-            currency:'IDR'
+            currency:'IDR',
+            notation: 'standard',
+            maximumFractionDigits: 0,
+            minimumFractionDigits: 0,
+            maximumSignificantDigits: 3,
+            minimumSignificantDigits: 3,
+            useGrouping: true,
         }
     ).format(amount)
 }
