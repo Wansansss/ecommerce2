191be59577ea275177e574c62c95f9a4786055ac
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-09 01:42:45 +0700

    Update page.tsx

--- src/app/product/kategori/[id]/page.tsx
+++ src/app/product/kategori/[id]/page.tsx
@@ -4,34 +4,23 @@ import NotFound from "@/components/utils/NotFound";
 import ProductCard from "@/components/utils/products/ProductCard";
 import { getProduct } from "@/libs/api";
 import React from "react";
-import Filter from "@/components/utils/Filter";
 
 const Page = async ({ params }: any) => {
   const id = params.id;
   const data = await getProduct("/category/list", `categorySecureId=${id}`);
-  // const filter = await getProduct("/filter",'')
 
   if (data.status !== "OK") {
     return <NotFound title="Product Tidak Ditemukan..." />;
   }
   return (
     <Container>
-      <div className="pt-44 font-bold">
-        <div className="">
-          <div className="h-full">
-            <Heading title="Filter Product" />
-            <Filter />
-          </div>
-          <div>
-            <div className="px-4 w-full">
-              <Heading title="Product" />
-              <hr className="h-1 bg-red-600" />
+      <div className="py-16">
+            <div className="py-16">
+              <Heading title="Pilihan Produk" center />
+              <hr className="h-1 bg-red-600 max-w-[50%] mx-auto" />
             </div>
             <ProductCard list={data} />
           </div>
-        </div>
-      </div>
-      <div></div>
     </Container>
   );
 };
