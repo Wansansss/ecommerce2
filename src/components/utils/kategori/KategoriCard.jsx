776b6d9a99b4d180eef76b08b755cdbb1628a3db
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-09 19:58:02 +0700

    Update KategoriCard.jsx

--- src/components/utils/kategori/KategoriCard.jsx
+++ src/components/utils/kategori/KategoriCard.jsx
@@ -10,17 +10,17 @@ const KategoriCard = ({ kategori }) => {
           <Link
             href={`/kategori/${kategoricard.categorySecureId}`}
             key={kategoricard.categorySecureId}
-            className="flex items-center justify-center flex-col border border-solid border-black hover:border-red-600 max-h-[300px] max-w-[200px]"
+            className="flex items-center justify-center flex-col border-[1px] rounded shadow-sm shadow-black border-black hover:border-red-600 hover:scale-105 duration-75 transition-all max-h-[300px] max-w-[200px]"
           >
             <Image
-              src="https://m.media-amazon.com/images/I/71p-tHQ0u1L._AC_SX679_.jpg"
+              src="http://89.116.134.204:8011/api/sl/v1/backoffice/uploadfile/files/ac1.jpeg"
               alt=""
               width={500}
               height={500}
               className="block p-1 max-h-[50%] max-w-[50%] hover:scale-100 hover:-translate-y-1 hover:-translate-x-1 hover:opacity-50 duration-300 transition-all"
             />
 
-            <div className="block h-[50%] p-2 w-full text-white font-semibold bg-red-600 hover:bg-neutral-800">
+            <div className="block h-[50%] p-2 w-full text-white font-semibold bg-red-600/90 hover:text-black/80">
               <p className="block text-center text-xl">
                 {kategoricard.categoryName}
               </p>
