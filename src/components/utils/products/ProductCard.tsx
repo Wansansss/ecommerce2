a946924099bf7ab26c3613b2fce52717469026ba
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-09 19:58:05 +0700

    Update ProductCard.tsx

--- src/components/utils/products/ProductCard.tsx
+++ src/components/utils/products/ProductCard.tsx
@@ -4,34 +4,45 @@ import { Rating } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import Container from "../Container";
+import { AiFillThunderbolt } from "react-icons/ai";
 
-const ProductCard = ({ list }:any) => {
+const ProductCard = ({ list }: any) => {
   return (
     <Container>
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-8 p-4 cursor-pointer">
-        {list.data?.map((data:any) => (
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 py-4 cursor-pointer">
+        {list.data?.map((data: any) => (
           <Link
             key={data.productSecureId}
             href={`/product/${data.productSecureId}`}
-            className="flex flex-col items-center justify-center border border-black hover:border-red-600 max-h-[350px] max-w-[500px] "
+            className="flex flex-col bg-slate-200 shadow-sm shadow-black rounded hover:scale-105 duration-300 transition-all "
           >
             <Image
-              className="block items-center justify-center max-h-[50%] max-w-[100%] object-fill border border-white p-1"
+              className="block items-center justify-center max-h-[200px] max-w-[100%] object-cover"
               src={data.fileUrlList[0].image}
               alt={data.productName}
-              width={100}
-              height={100}
+              width={500}
+              height={200}
             />
-            <div className="max-w-[90%] mt-4 text-center sm:text-md">
+            <div className="text-center font-semibold">
               {truncateText(data.productName)}
             </div>
-            <div className=" max-w-[90%] mt-4 text-center">
+            <div className="text-center">
               {truncateText(data.productDescription)}
             </div>
-            <div className="text-center max-w-[100%] max-h-[50%] sm:text-md">
-              <Rating value={data.productRating} readOnly size="small"/>
+            <div className="text-center">
+              <Rating value={data.productRating} readOnly size="small" />
             </div>
-            <div className="text-center justify-center font-semibold">{formatPrice(data.amount)}</div>
+            <div className="text-center font-semibold">
+              <h1 className="text-lg">{formatPrice(data.amountDiscount)}</h1>
+              <div className="flex flex-row text-center justify-center">
+                <h1 className="text-md line-through text-red-600">
+                  {formatPrice(data.amount)}
+                </h1>
+                <h1 className="flex px-1 text-sm text-center text-red-600">
+                 -{data.discount}
+                </h1>
+              </div>
+            </div>
           </Link>
         ))}
       </div>
