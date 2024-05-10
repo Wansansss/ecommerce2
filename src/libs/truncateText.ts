7c8d57f824b3465b4d65858e558e8bd0895b4705
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-11 00:11:08 +0700

    Update truncateText.ts

--- src/libs/truncateText.ts
+++ src/libs/truncateText.ts
@@ -1,6 +1,7 @@
 export const truncateText =
 (str: string) => {
-    if(str.length < 25) return str;
+    if(str.length < 15) return str;
 
-    return str.substring(0, 25) + '...';
+    return str.substring(0, 15) + '...';
 }
+
