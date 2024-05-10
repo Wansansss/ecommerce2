88026d52d7a57bfa36906bfd1fc54996a01e2253
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-11 00:10:43 +0700

    Update ProductCard.tsx

--- src/components/utils/products/ProductCard.tsx
+++ src/components/utils/products/ProductCard.tsx
@@ -1,44 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
 import { formatPrice } from "@/libs/formatPrice";
 import { truncateText } from "@/libs/truncateText";
 import { Rating } from "@mui/material";
-import Image from "next/image";
 import Link from "next/link";
-import Container from "../Container";
-import { AiFillThunderbolt } from "react-icons/ai";
 
 const ProductCard = ({ list }: any) => {
   return (
-    <Container>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 py-4 cursor-pointer">
         {list.data?.map((data: any) => (
           <Link
             key={data.productSecureId}
             href={`/product/${data.productSecureId}`}
-            className="flex flex-col bg-slate-200 shadow-sm shadow-black rounded hover:scale-105 duration-300 transition-all "
+            className="flex flex-col border hover:border-red-700 bg-rose-100 shadow shadow-black/60 rounded hover:scale-105 duration-300 transition-all w-full"
           >
-            <Image
-              className="block items-center justify-center max-h-[200px] max-w-[100%] object-cover"
-              src={data.fileUrlList[0].image}
+            <img
+              className="block items-center justify-center max-h-[200px] min-h-[200px] object-cover"
+              src={data.fileUrlList[0]?.image}
               alt={data.productName}
-              width={500}
-              height={200}
             />
-            <div className="text-center font-semibold">
+            <div className="py-2 pl-4 font-semibold">
               {truncateText(data.productName)}
             </div>
-            <div className="text-center">
+            <hr className="h-[2px] w-[90%] mx-auto bg-red-600" />
+            <div className="pl-4">
               {truncateText(data.productDescription)}
             </div>
-            <div className="text-center">
-              <Rating value={data.productRating} readOnly size="small" />
+            <div className="pl-4">
+              <Rating value={data.ratingByClick} readOnly size="small" />
             </div>
-            <div className="text-center font-semibold">
-              <h1 className="text-lg">{formatPrice(data.amountDiscount)}</h1>
-              <div className="flex flex-row text-center justify-center">
-                <h1 className="text-md line-through text-red-600">
-                  {formatPrice(data.amount)}
+            <div className="font-semibold pl-4 py-4">
+              <h1 className="text-md">{formatPrice(data.amountDiscount)}</h1>
+              <div className="flex flex-row text-wrap">
+                <h1 className="text-xs line-through text-gray-500">
+                {formatPrice(data.amount)}
                 </h1>
-                <h1 className="flex px-1 text-sm text-center text-red-600">
+                <h1 className="flex px-1 text-xs text-center text-red-600">
                  -{data.discount}
                 </h1>
               </div>
@@ -46,7 +42,6 @@ const ProductCard = ({ list }: any) => {
           </Link>
         ))}
       </div>
-    </Container>
   );
 };
 
