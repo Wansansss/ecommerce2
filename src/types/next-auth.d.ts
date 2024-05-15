d462fb4b87a3c44f87f666ee3a42177492676f59
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-16 04:08:10 +0700

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,7 +7,7 @@ declare module "next-auth" {
   interface Session {
     user: {
       role: string | undefined | null
-      secureId:string | undefined | null
+      secureId:string
       fullName:string | undefined | null
       address:string | undefined | null
     } 
