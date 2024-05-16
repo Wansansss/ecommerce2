c89240aaff33fc67391b99b1a3743359b66a2125
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-16 20:43:20 +0700

    Update CartClient.tsx

--- src/app/cart/CartClient.tsx
+++ src/app/cart/CartClient.tsx
@@ -3,70 +3,168 @@ import Button from "@/components/utils/Button";
 import Heading from "@/components/utils/Heading";
 import { useCart } from "@/hooks/useCart";
 import Link from "next/link";
-import { MdArrowBack } from "react-icons/md";
+import { MdArrowBack, MdDone } from "react-icons/md";
 import ItemContent from "./ItemContent";
 import { formatPrice } from "@/libs/formatPrice";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
-import axios from "axios";
 import { useSession } from "next-auth/react";
-
+// import Modal from 'react-modal';
+import Box from "@mui/material/Box";
+// import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Image from "next/image";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 const CartClient = () => {
   const { cartProducts, handleClearCart, cartTotalAmount } = useCart();
   const [loading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const {data: session,status} = useSession()
-  let secureId = ''
-    if (session?.user?.secureId)
-        secureId = session.user?.secureId
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [orderId, setOrderId] = useState("");
+  const { data: session, status } = useSession();
+  let secureId = "";
+  if (session?.user?.secureId) secureId = session.user?.secureId;
 
   const router = useRouter();
+
+  const handleClose = () => setIsModalOpen(false);
+
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   const handleCheckout = async () => {
     if (cartProducts) {
       {
-        cartProducts.map((item) => {
-          setIsLoading(true);
-          setError(error);
-          fetch(process.env.NEXT_PUBLIC_API_URL + "/api/sl/v1/web/transaction/check-out",{
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "x-user-secure-id": secureId,
-            },
-            body: JSON.stringify({
-              productSecureId: item.productSecureId,
-              totalItemsPurchased: item.qty
-            })
-          })
-            .then((response) => {
+        cartProducts.map(async (item) => {
+          await fetch(
+            process.env.NEXT_PUBLIC_API_URL +
+              "/api/sl/v1/web/transaction/check-out",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "x-user-secure-id": secureId,
+              },
+              body: JSON.stringify([
+                {
+                  productSecureId: item.productSecureId,
+                  totalItemsPurchased: item.qty,
+                },
+              ]),
+            }
+          )
+            .then(async (response) => {
               setIsLoading(true);
-              if (response.ok) {
-                toast.success("Pesanan Telah diterima")
-                // handleClearCart()
-                setIsLoading(false)
-                console.log(response)
-                // router.push("https://wa.me/6282137026688?text=Hallo Admin,Apakah pesanan saya sudah diterima?")
-              } else {
-                toast.error("Silahkan Login terlebih dahulu");
-                return router.push("/login");
+              if (response.status === 200) {
+                const data = await response.json();
+                toast.success("Pesanan Telah diterima");
+                handleClearCart();
+                setIsLoading(false);
+                setOrderId(data.data[0].orderId);
+              } else if (response.status === 400) {
+                const data = await response.json();
+                toast(
+                  (t) => (
+                    <span className="flex flex-col">
+                      {data.message}
+                      <div className="flex flex-row gap-1">
+                        <Link
+                          href={"https://wa.me/6282137026688?text=Hallo Admin,"}
+                          target="blank"
+                          className="bg-green-600 p-2"
+                          onClick={() => toast.dismiss(t.id)}
+                        >
+                          OK
+                        </Link>
+                        <Link
+                          href={"/cart"}
+                          className="bg-red-600 p-2"
+                          onClick={() => toast.dismiss(t.id)}
+                        >
+                          Batal
+                        </Link>
+                      </div>
+                    </span>
+                  ),
+                  {
+                    icon: <MdDone size={20} />,
+                  }
+                );
+              } else if (response.status === 404) {
+                toast.error("Silahkan Login Terlebih Dahulu");
               }
             })
             .catch((error) => {
-              setError(true);
               console.log(error);
               toast.error("Terjadi Kesalahan");
+            })
+            .finally(() => {
+              // handleClearCart();
             });
         });
       }
     }
   };
+  console.log(orderId);
 
   if (!cartProducts || cartProducts.length === 0) {
     return (
       <div className=" flex flex-col items-center mt-40 mx-8 mb-10">
+        {orderId && (
+          <Modal
+            open={isModalOpen}
+            // onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Informasi Pembayaran
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Silahkan lakukan pembayaran ke rekening berikut ini
+                <div className="flex flex-row gap-2 pt-4 pb-4">
+                  <Image
+                    src="/assets/img/Bank_Central_Asia.svg"
+                    alt=""
+                    width={100}
+                    height={100}
+                  />
+                  <h1>647 058 2756 a.n LESTARI</h1>
+                </div>
+                Kemudian kirimkan bukti pembayaran melalui nomer WA Admin
+                dibawah ini
+                <div className="flex flex-row gap-2 pt-4 pb-4">
+                  <Link
+                    href={"https://wa.me/6281290844844?text=Hallo Admin,"}
+                    target="blank"
+                    className="font-medium"
+                  >
+                    <div className="flex">
+                      <IoLogoWhatsapp size={18} className="text-green-600 rounded-xl" />
+                      <h1 className="pl-1">0812-9084-4844.</h1>
+                    </div>
+
+                    <h2 className="hover:text-red-600 transition-all duration-300">
+                      Klik Disini
+                    </h2>
+                  </Link>
+                </div>
+              </Typography>
+            </Box>
+          </Modal>
+        )}
         <div className="text-2xl">Tidak Ada Product</div>
         <div>
           <Link
