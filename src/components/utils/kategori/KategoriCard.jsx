b6bebc4bb10fe93714e381bf5c4a39f0bdf6055d
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-16 20:43:25 +0700

    Update KategoriCard.jsx

--- src/components/utils/kategori/KategoriCard.jsx
+++ src/components/utils/kategori/KategoriCard.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import Container from "../Container";
 
@@ -12,12 +12,12 @@ const KategoriCard = ({ kategori }) => {
             key={kategoricard.categorySecureId}
             className="flex items-center justify-center flex-col border-[1px] rounded shadow-sm shadow-black border-black hover:border-red-600 hover:scale-105 duration-75 transition-all max-h-[300px] max-w-[300px]"
           >
-            <Image
+            <img
               src={kategoricard.fileUrl[0]}
-              alt=""
+              alt="produk"
               width={500}
               height={500}
-              className="block p-1 max-h-[50%] max-w-[50%] hover:scale-100 hover:-translate-y-1 hover:-translate-x-1 hover:opacity-50 duration-300 transition-all"
+              className="block p-1 max-h-[50%] max-w-[50%] hover:scale-100 hover:-translate-y-1 hover:-translate-x-1 hover:opacity-50 duration-300 transition-all object-fill"
             />
 
             <div className="block h-[50%] p-2 w-full text-white font-semibold bg-red-600/90 hover:text-black/80">
