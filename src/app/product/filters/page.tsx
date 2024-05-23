dbe67d4a695c4d83787ea73315ab8baaf8978e7a
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-23 13:14:50 +0700

    Update page.tsx

--- src/app/product/filters/page.tsx
+++ src/app/product/filters/page.tsx
@@ -2,56 +2,82 @@
 
 import Container from "@/components/utils/Container";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import Filter from "./Filter";
+import { useState } from "react";
 import ProductCard from "@/components/utils/products/ProductCard";
 import Heading from "@/components/utils/Heading";
+import Button from "@/components/utils/Button";
+import toast from "react-hot-toast";
 
-type Props = {};
 
-const FilterPage = (props: Props) => {
-  const [price, setPrice] = useState({
-    rangeFrom: 0,
-    rangeTo: 0,
-  });
-  const [response, setResponse] = useState<any[]>([]);
+const FilterPage = () => {
+  const [minPrice, setMinPrice] = useState<any>("0");
+  const [response, setResponse] = useState<any>("");
+  const [maxPrice, setMaxPrice] = useState<any>("0");
+  // console.log(minPrice)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios
-          .get("/api/filterProduct", {
-            params: {
-              price: {
-                rangeFrom: price.rangeFrom,
-                rangeTo: price.rangeTo,
-              },
-            },
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-          .then((response) => {
-            console.log("Response: " + response.data);
-            setResponse(response.data);
-          });
-      } catch (err) {
-        console.log("Error: " + err);
-      }
-    };
-    fetchData();
-  }, [price]);
+  async function handleSubmit(event: any) {
+    event.preventDefault();
+    await axios
+      .get(
+        process.env.NEXT_PUBLIC_API_URL +
+          `/api/sl/v1/web/product/filter?page=1&limit=10&rangeFrom=${minPrice}&rangeTo=${maxPrice}`
+      )
+      .then((response) => {
+        if (response.status === 200) {
+          setResponse(response.data);
+        }
+      })
+      .catch(() => {
+        toast.error("Produk Tidak Ditemukan");
+      });
+  }
   return (
     <Container>
       <div className="py-16">
         <div className=" py-16">
           <Heading title="Filter" />
-          <Filter price={price} setPrice={setPrice} />
+          <div className="border-b-[0.5px] pb-10">
+            <div className="px-5 py-4 border-b-[0.5px] mb-5">
+              <h1 className="text-neutral-800">Harga</h1>
+            </div>
+            <div className="grid grid-cols-2 gap-5 px-5 overflow-hidden">
+              <div>
+                <label htmlFor="" className="text-[15px] opacity-75">
+                  Dari
+                </label>
+                <div className="relative">
+                  <span className="absolute left-3 top-1">Rp</span>
+                  <input
+                    type="number"
+                    className="w-[200px] outline-none border-[1px] border-black rounded px-2 text-center py-[2px]"
+                    name="rangeFrom"
+                    onChange={(e) => setMinPrice(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-5 px-5 overflow-hidden">
+              <div>
+                <label htmlFor="" className="text-[15px] opacity-75">
+                  Sampai
+                </label>
+                <div className="relative">
+                  <span className="absolute left-3 top-1">Rp</span>
+                  <input
+                    type="number"
+                    className="w-[200px] outline-none border-[1px] border-black rounded px-2 text-center py-[2px]"
+                    name="rangeTo"
+                    onChange={(e) => setMaxPrice(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="py-10">
+              <Button label="Simpan" small outline onClick={handleSubmit} />
+            </div>
+          </div>
           <Heading title="Produk Pilihan" />
           <ProductCard list={response} />
-        </div>
-        <div className="px-10">
-          
         </div>
       </div>
     </Container>
