182221bddeb88acaf16da8c2b02decd36c508b6a
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-23 13:15:09 +0700

    Update page.tsx

--- src/app/user/formAlamat/page.tsx
+++ src/app/user/formAlamat/page.tsx
@@ -1,14 +1,12 @@
-import { getSession } from "@/actions/getCurrentUser";
 import Container from "@/components/utils/Container";
 import FormWrap from "@/components/utils/FormWrap";
 import FormAlamat from "@/components/utils/form/FormAlamat";
 
 const page = () => {
-    const user = getSession()
     return (  
         <Container>
             <FormWrap>
-                <FormAlamat currentUser={user}/>
+                <FormAlamat/>
             </FormWrap>
         </Container>
     );
