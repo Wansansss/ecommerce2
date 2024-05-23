89edba23fb1b6e02d948dc564fcd1f4b4c0e001b
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-23 13:14:59 +0700

    Update page.tsx

--- src/app/user/dashboard/alamat/page.tsx
+++ src/app/user/dashboard/alamat/page.tsx
@@ -1,26 +1,54 @@
-import { getSession } from "@/actions/getCurrentUser";
+'use client'
 import Button from "@/components/utils/Button";
 import Container from "@/components/utils/Container";
 import FormWrap from "@/components/utils/FormWrap";
 import Heading from "@/components/utils/Heading";
-import { getAccountDetail } from "@/libs/api";
-import { truncateText } from "@/libs/truncateText";
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { IoMdArrowRoundBack } from "react-icons/io";
 
-const UserCard = async () => {
-  const session = await getSession();
-  const currentUser = await getAccountDetail(session?.secureId);
+const UserCard = () => {
+  const [secureId,setSecureId] = useState('')
+  const [currentUser,setCurrentUser] = useState<any>([])
+  const router = useRouter()
+  useEffect(()=> {
+    let usersecureId = sessionStorage.getItem('secureId');
+    if(usersecureId){
+      setSecureId(usersecureId)
+    }else{
+      router.push('/login')
+    }
+  },[router])
+
+ 
+
+  function getAccountDetail(){
+    const config = {
+      headers: {
+        "x-user-secure-id": secureId as string,
+        "accept": "*/*"
+      },
+    }
+       axios.get(process.env.NEXT_PUBLIC_API_URL+`/api/sl/v1/web/users/account/detail`,config)
+      .then((response) =>{
+        console.log(response)
+        setCurrentUser(response.data.data)
+      }).catch((err) =>{console.log(err)});
+    }
+     useEffect(getAccountDetail,[secureId])
+
+
 
   return (
     <Container>
       <div className="py-24 w-full">
-        <div className="px-8 flex md:flex-col items-center justify-center gap-4 text-slate-500 border-slate-500">
+        <div className="py-4 px-8 flex md:flex-col items-center justify-center gap-4 text-slate-500 border-slate-500">
           <FaUserCircle size={100} />
           <div className="px-4 items-center justify-center text-3xl font-bold text-black">
-            Welcome,{currentUser.data.accountName}
+            Welcome,{currentUser?.accountName}
           </div>
         </div>
         <FormWrap>
@@ -28,13 +56,13 @@ const UserCard = async () => {
           <div className="w-full font-semibold">
             <div className="grid grid-cols-1 gap-4 break-all">
               <h1>
-                Alamat: {currentUser.data.address.address1}
+                Alamat: {currentUser?.address?.address1}
               </h1>
-              <h1>Kelurahan: {currentUser.data.address.village}</h1>
-              <h1>Kota: {currentUser.data.address.city}</h1>
-              <h1>Kecamatan: {currentUser.data.address.subdistrict}</h1>
-              <h1>Provinsi: {currentUser.data.address.province}</h1>
-              <h1>Kode Pos: {currentUser.data.address.postalCode}</h1>
+              <h1>Kelurahan: {currentUser?.address?.village}</h1>
+              <h1>Kota: {currentUser?.address?.city}</h1>
+              <h1>Kecamatan: {currentUser?.address?.subdistrict}</h1>
+              <h1>Provinsi: {currentUser?.address?.province}</h1>
+              <h1>Kode Pos: {currentUser?.address?.postalCode}</h1>
             </div>
           </div>
         </FormWrap>
