44b4022b8f747ba6dfb74dbcd0338477413ddc33
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-23 13:15:03 +0700

    Update page.tsx

--- src/app/user/dashboard/historyTransaksi/page.tsx
+++ src/app/user/dashboard/historyTransaksi/page.tsx
@@ -1,19 +1,32 @@
-import { getSession } from "@/actions/getCurrentUser";
+'use client'
 import Heading from "@/components/utils/Heading";
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { IoMdArrowRoundBack } from "react-icons/io";
 
-const UserCard = async () => {
-  const currentUser = await getSession();
+const UserCard = () => {
+  const [fullName,setfullName]= useState('')
+  const router = useRouter();
+
+  useEffect(() => {
+    let fullName:any = sessionStorage.getItem("fullName");
+    setfullName(fullName);
+    if(!fullName){
+      router.push('/login')
+    }
+  }, [fullName,router]);
+
+
 
   return (
     <div className="py-16">
       <div className="py-12 px-8 flex md:flex-col items-center justify-center gap-4 text-slate-500 border-slate-500">
         <FaUserCircle size={100} />
         <div className="px-4 items-center justify-center text-3xl font-bold text-black">
-          Welcome,{currentUser?.fullName}
+          Welcome,{fullName}
         </div>
       </div>
       <div className="flex flex-col items-center justify-center px-24 gap-2 text-xl font-bold text-black">
