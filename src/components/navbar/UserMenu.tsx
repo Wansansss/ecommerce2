281edcb1faf11100a0d3948d3de2cd32be6ac5ae
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-23 13:15:20 +0700

    Update UserMenu.tsx

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -1,25 +1,61 @@
 "use client";
 
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { AiFillCaretDown } from "react-icons/ai";
 import Avatar from "../utils/Avatar";
 import Link from "next/link";
 import MenuItem from "./MenuItem";
-import { signOut } from "next-auth/react";
 import BackDrop from "./BackDrop";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
-interface UserMenuProps {
-  currentUser?: any;
-}
 
-const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+
+const UserMenu= () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState<any>([])
   const router = useRouter();
 
   const toggleOpen = useCallback(() => {
     setIsOpen((prev) => !prev);
   }, []);
+  const [secureId,setSecureId] = useState('')
+  useEffect(()=> {
+    let usersecureId = sessionStorage.getItem('secureId');
+    if(usersecureId){
+      setSecureId(usersecureId)
+    }
+  },[])
+
+  function getAccountDetail(){
+    const config = {
+      headers: {
+        "x-user-secure-id": secureId as string,
+        "accept": "*/*"
+      },
+    }
+       axios.get(process.env.NEXT_PUBLIC_API_URL+`/api/sl/v1/web/users/account/detail`,config)
+      .then((response) =>{
+        console.log(response)
+        setCurrentUser(response.data.data)
+      }).catch((err) =>{console.log(err)});
+    }
+     useEffect(getAccountDetail,[secureId])
+
+     function handleLogout() {
+      axios.post(process.env.NEXT_PUBLIC_API_URL + '/api/sl/v1/web/users/signout')
+      .then((response) => {
+        sessionStorage.clear();
+        return response
+      }).catch((error) => {
+        console.log(error);
+      }).finally(()=>{
+        router.push('/login')
+        setTimeout(()=>{
+          window.location.reload()
+        },1000)
+      })
+    };
 
   return (
     <>
@@ -35,29 +71,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
         </div>
         {isOpen && (
           <div className="absolute rounded-md shadow-lg shadow-black w-[170px] bg-white overflow-hidden right-0 top-12 text-sm flex flex-col cursor-pointer">
-            {currentUser ? (
-              <div>
-                <Link
-                  href="/user/dashboard/historyTransaksi"
-                  className="hover:text-red-600"
-                >
-                  <MenuItem onClick={toggleOpen}> Your Orders </MenuItem>
-                </Link>
-                <Link href="/user/dashboard" className="hover:text-red-600">
-                  <MenuItem onClick={toggleOpen}>Dashboard </MenuItem>
-                </Link>
-                {/* <Link href="/login" className="hover:text-red-600"> */}
-                <MenuItem
-                  onClick={() => {
-                    toggleOpen();
-                    signOut({ callbackUrl: "/login" });
-                  }}
-                >
-                  Logout
-                </MenuItem>
-                {/* </Link> */}
-              </div>
-            ) : (
+            {!secureId ? (
               <div>
                 <Link href="/login" className="hover:text-red-600">
                   <MenuItem onClick={toggleOpen}> Login </MenuItem>
@@ -65,6 +79,19 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <Link href="/register" className="hover:text-red-600">
                   <MenuItem onClick={toggleOpen}> Register </MenuItem>
                 </Link>
+              </div>
+            ):(
+              <div>
+                <Link href="/user/dashboard" className="hover:text-red-600">
+                  <MenuItem onClick={toggleOpen}>Dashboard </MenuItem>
+                </Link>
+                {/* <Link href="/login" className="hover:text-red-600"> */}
+                <MenuItem
+                  onClick={() => {handleLogout()}}
+                >
+                  Logout
+                </MenuItem>
+                {/* </Link> */}
               </div>
             )}
             
