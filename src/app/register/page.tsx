871565f6f33dd57c1e106603ef0238fd7bbc88f9
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-23 13:14:52 +0700

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,15 +1,13 @@
-import { getSession } from "@/actions/getCurrentUser";
 import Container from "../../components/utils/Container";
 import FormWrap from "../../components/utils/FormWrap";
 import RegisterForm from "./RegisterForm";
 
 const Register = async() => {
-    const user = await getSession();
     return ( 
         <Container>
             <div className="py-16">
             <FormWrap>
-                <RegisterForm currentUser={user}/>
+                <RegisterForm/>
             </FormWrap>
             </div>
             
