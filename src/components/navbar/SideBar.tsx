d66b89fd36d4dab732a58340f2f9c992ef2d55fd
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-28 02:42:47 +0700

    Update SideBar.tsx

--- src/components/navbar/SideBar.tsx
+++ src/components/navbar/SideBar.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable @next/next/no-async-client-component */
 "use client";
-import React, { useState } from "react";
-import SingleNav from "./SingleNav";
+import React, { useEffect, useState } from "react";
 import InputSearch from "./InputSearch";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
 import { MdClose, MdMenu } from "react-icons/md";
 import BackDrop from "./BackDrop";
-
+import axios from "axios";
+import { PiCaretDown } from "react-icons/pi";
+import Link from "next/link";
 
 type NavItem = {
   label: string;
@@ -25,112 +26,105 @@ const navItems: NavItem[] = [
     link: "/product/promo",
   },
   {
-    label: "Contact Us",
+    label: "Kontak",
     link: "/contact",
+  },
+  {
+    label: "Kategori Produk",
+    children: [
+      {
+        label: "Populer",
+        link: "/populer",
+      },
+      {
+        label: "Terbaru",
+        link: "/terbaru",
+      },
+      {
+        label: "Terlaris",
+        link: "/terlaris",
+      },
+    ],
   },
 ];
 
-
-
-const SideBar= () => {
+const SideBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const handleNav = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const [kategori, setKategori] = useState<any>(['']);
+
+  function getKategori() {
+    axios
+      .get(
+        process.env.NEXT_PUBLIC_API_URL + `/api/sl/v1/web/product-category/list`
+      )
+      .then((response) => {
+        // console.log(response)
+        setKategori(response.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(getKategori, []);
+  console.log(kategori)
 
   return (
     <div className="flex flex-row items-center justify-center px-4">
       <div
         className={
           menuOpen
-            ? "flex flex-col items-start gap-y-12 fixed top-20 right-8 p-8 bg-white rounded-3xl shadow-md w-64 transition-all duration-300 z-50"
-            : "flex flex-col items-start gap-y-12 fixed top-20 p-8 bg-white rounded-3xl shadow-md w-64 transition-all duration-300 z-50 -right-[100%]"
+            ? "flex flex-col items-start gap-y-2 fixed top-20 right-8 p-8 bg-white rounded-3xl shadow-md w-64 transition-all duration-300 z-50"
+            : "flex flex-col items-start gap-y-2 fixed top-20 p-8 bg-white rounded-3xl shadow-md w-64 transition-all duration-300 z-50 -right-[100%]"
         }
       >
-        
-        {navItems.map((d, i) => (
-          <SingleNav key={i} label={d.label} link={d.link} />
-        ))}
-        <InputSearch/>
+        <InputSearch />
+         {navItems.map((link, i) => (
+        <div key={i} className="relative group px-2 py-3 transition-all">
+          <div className="flex cursor-pointer items-center gap-2 group-hover:text-red-600 transition-all duration-500">
+            <Link href={link.link ?? "/"}>{link.label}</Link>
+
+            {link.children && (
+              <PiCaretDown className=" rotate-180 transition-all duration-500 group-hover:rotate-0" />
+            )}
+          </div>
+          {/* dropdown */}
+
+          {link.children && (
+            <div className="absolute right-0 top-10 hidden w-auto flex-col gap-1 bg-white py-3 px-6 shadow-md transition-all duration-500 group-hover:flex">
+              {kategori?.map((ch: any, index: any) => (
+                <Link
+                  key={index}
+                  href={`/product/kategori/${ch.categorySecureId}`}
+                  className="flex cursor-pointer py-1 pl-6 pr-8 text-black/90 hover:text-red-600"
+                >
+                  <ul className="list-disc hover:list-none">
+                    <li>{ch.categoryName}</li>
+                  </ul>
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
       </div>
-      <div className="md:hidden xl:hidden cursor-pointer text-3xl hover:text-red-600 transition-all" 
-      onClick={handleNav}>{!menuOpen ? 
-      <MdMenu /> 
-      : 
-      <MdClose/>}</div>
-      <div className="px-2 md:hidden xl:hidden">
-      <CartCount />
+      <div
+        className="md:hidden xl:hidden cursor-pointer text-3xl hover:text-red-600 transition-all"
+        onClick={handleNav}
+      >
+        {!menuOpen ? <MdMenu /> : <MdClose />}
       </div>
       <div className="px-2 md:hidden xl:hidden">
-      <UserMenu/>
+        <CartCount />
+      </div>
+      <div className="px-2 md:hidden xl:hidden">
+        <UserMenu />
       </div>
       {menuOpen ? <BackDrop onClick={handleNav} /> : null}
     </div>
-
-    // <>
-    //   <div className="flex flex-row items-center gap-4">
-    //     <div className="flex md:hidden">
-    //       <InputSearch />
-    //     </div>
-    //     <div className="md:hidden">
-    //       <CartCount />
-    //     </div>
-
-    //     <button
-    //       onClick={handleNav}
-    //       className="hover:text-red-600 transition-all duration-500 md:hidden lg:hidden xl:hidden"
-    //     >
-    //       <FiMenu size={25} />
-    //     </button>
-    //   </div>
-
-    //   <div
-    //     className={
-    //       menuOpen
-    //         ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-white p-10 ease-in duration-500"
-    //         : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
-    //     }
-    //   >
-    //     <div className="gap-4 p-4 flex flex-col h-screen overflow-y-auto">
-    //       <div className="flex justify-end items-center hover:text-red-600 cursor-pointer text-black ">
-    //         <AiOutlineClose size={32} onClick={handleNav} />
-    //       </div>
-    //       <div className="flex flex-col text-base gap-2 transition-all">
-    //         {navItems.map((d, i) => (
-    //           <SingleNav key={i} label={d.label} link={d.link} />
-    //         ))}
-    //       </div>
-    //       {!currentUser ? (
-    //         <div className="flex flex-col mt-6 text-base items-center gap-1">
-    //           <Link
-    //             href={"/login"}
-    //             className="flex items-center justify-center shadow-lg shadow-black bg-red-600 w-full rounded-xl hover:bg-black hover:text-white transition-all duration-300 p-2 font-semibold hover:scale-105"
-    //           >
-    //             Login
-    //           </Link>
-    //           <Link
-    //             href={"/register"}
-    //             className="flex items-center justify-center border w-full rounded-xl bg-red-600 hover:bg-black hover:text-white transition-all duration-300 shadow-lg shadow-white p-2 font-semibold hover:scale-105"
-    //           >
-    //             Register
-    //           </Link>
-    //         </div>
-    //       ) : (
-    //         <div className="flex flex-col mt-6 text-base items-center gap-1 cursor-pointer">
-    //           <div className="flex items-center justify-center border w-full rounded-xl bg-red-600 hover:bg-black hover:text-white transition-all duration-300 shadow-lg shadow-white font-semibold hover:scale-105">
-    //             <MenuItem
-    //               onClick={() => {
-    //                 signOut();
-    //               }}
-    //             >
-    //               Logout
-    //             </MenuItem>
-    //           </div>
-    //         </div>
-    //       )}
-    //     </div>
-    //   </div>
-    // </>
   );
 };
 
