a0cddb34da911c60e7207104c66ed09ed454cc89
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-27 09:21:05 +0700

    Update ProductCard.tsx

--- src/components/utils/products/ProductCard.tsx
+++ src/components/utils/products/ProductCard.tsx
@@ -2,6 +2,7 @@
 import { formatPrice } from "@/libs/formatPrice";
 import { truncateText } from "@/libs/truncateText";
 import { Rating } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 
 const ProductCard = ({ list }: any) => {
@@ -13,8 +14,10 @@ const ProductCard = ({ list }: any) => {
             href={`/product/${data.productSecureId}`}
             className="flex flex-col border hover:border-red-700 bg-rose-100 shadow shadow-black/60 rounded hover:scale-105 duration-300 transition-all w-[100%]"
           >
-            <img
+            <Image
               className="block items-center justify-center max-h-[200px] min-h-[200px] object-fill"
+              width={1920}
+              height={1080}
               src={data.fileUrlList[0]?.image}
               alt={data.productName}
             />
