dc1edd21158d6215b742c4579a7243b9cf3618a0
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-05-29 12:04:41 +0700

    Update ProductCard.tsx

--- src/components/utils/products/ProductCard.tsx
+++ src/components/utils/products/ProductCard.tsx
@@ -7,44 +7,52 @@ import Link from "next/link";
 
 const ProductCard = ({ list }: any) => {
   return (
-      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 py-4 cursor-pointer">
-        {list.data?.map((data: any) => (
-          <Link
-            key={data.productSecureId}
-            href={`/product/${data.productSecureId}`}
-            className="flex flex-col border hover:border-red-700 bg-rose-100 shadow shadow-black/60 rounded hover:scale-105 duration-300 transition-all w-[100%]"
-          >
-            <Image
-              className="block items-center justify-center max-h-[200px] min-h-[200px] object-fill"
-              width={1920}
-              height={1080}
-              src={data.fileUrlList[0]?.image}
-              alt={data.productName}
-            />
-            <div className="py-2 pl-4 font-semibold">
-              {truncateText(data.productName)}
+    <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 py-4 cursor-pointer">
+      {list.data?.map((data: any) => (
+        <Link
+          key={data.productSecureId}
+          href={`/product/${data.productSecureId}`}
+          className="flex flex-col border hover:border-red-700 bg-rose-100 shadow shadow-black/60 rounded hover:scale-105 duration-300 transition-all w-[100%]"
+        >
+          <Image
+            className="block items-center justify-center max-h-[200px] min-h-[200px] object-fill"
+            width={1920}
+            height={1080}
+            src={data.fileUrlList[0]?.image}
+            alt={data.productName}
+          />
+          <div className="py-2 pl-4 font-semibold">
+            {truncateText(data.productName)}
+          </div>
+          <hr className="h-[2px] w-[90%] mx-auto bg-red-600" />
+          <div className="pl-4">{truncateText(data.productDescription)}</div>
+          <div className="pl-4">
+            <Rating value={data.ratingByClick} readOnly />
+          </div>
+          {data.discount === 0 ? (
+            <div className="font-semibold pl-4 py-3">
+              <h1 className="text-md">{`${formatPrice(
+                data.amount
+              )}`}</h1>
             </div>
-            <hr className="h-[2px] w-[90%] mx-auto bg-red-600" />
-            <div className="pl-4">
-              {truncateText(data.productDescription)}
-            </div>
-            <div className="pl-4">
-              <Rating value={data.ratingByClick} readOnly size="small" />
-            </div>
-            <div className="font-semibold pl-4 py-4">
-              <h1 className="text-md">{`${formatPrice(data.amountDiscount)}`}</h1>
+          ) : (
+            <div className="font-semibold pl-4 py-3">
+              <h1 className="text-md">{`${formatPrice(
+                data.amountDiscount
+              )}`}</h1>
               <div className="flex flex-row text-wrap">
                 <h1 className="text-xs line-through text-gray-500">
-                {formatPrice(data.amount)}
+                  {formatPrice(data.amount)}
                 </h1>
                 <h1 className="flex px-1 text-xs text-center text-red-600">
-                 -{data.discount}%
+                  -{data.discount}%
                 </h1>
               </div>
             </div>
-          </Link>
-        ))}
-      </div>
+          )}
+        </Link>
+      ))}
+    </div>
   );
 };
 
