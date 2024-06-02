993419f4e3d665507c911e5d8b840d4788018c04
Wansansss <113201428+Wansansss@users.noreply.github.com>
2024-06-02 16:23:22 +0700

    Update Input.tsx

--- src/components/utils/inputusers/Input.tsx
+++ src/components/utils/inputusers/Input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FieldElement, FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 
 interface InputProps {
